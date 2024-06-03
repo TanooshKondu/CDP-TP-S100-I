4fbcff90eca84c8d27c33c2fa7b23b47eb6581b0
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-06-03 15:50:33 +0530

    updated

--- Day-1/BinaryString.py
+++ Day-1/BinaryString.py
@@ -9,7 +9,8 @@ n = int(input())
 prob(0, " ",n)
     
 #PRACTICE PROBLEMS
+#https://pastebin.com/2yYrb7yN
 
 #1. WRITE A PYTHON PROGRAM, TO ACCEPT A LIST OF INTEGERS AND PRINT THE SUM OF ALL 5 DIVISIBLE NUMBERS USING RECURSION
 
-#2.WRITE A PYTHON PROGRAM, TO PRINT ALL EVEN INDEXED
+#2. write a python program to print all even indexed elements in both left to right and right to left manner using recursion.
