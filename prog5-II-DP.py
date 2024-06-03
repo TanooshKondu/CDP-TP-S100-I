33ff4616bed7bbe8c2ee6669add24721a2e1dcfe
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-06-03 14:57:08 +0530

    updated

--- prog5-II-DP.py
+++ prog5-II-DP.py
@@ -2,6 +2,7 @@
 #https://pastebin.com/fZiLtTAF
 #using Dynamic Programming
 # Passing data from child function to Parent function
+#https://pastebin.com/yG8V03SM
 def progMax(index, n, nums):
     if index == n-1:
         return nums[index]
@@ -11,8 +12,17 @@ def progMax(index, n, nums):
     else:
         return nextMax
     
+def progMin(index, n, nums):
+    if index == n-1:
+        return nums[index]
+    nextMin = progMax(index+1, n, nums)
+    if nums[index] < nextMin:
+        return nums[index]
+    else:
+        return nextMin
 index = 0
 nums = [12, 34, 12, 5, 7]
 n = len(nums)
 result = progMax(0, n, nums)
-print("max is: ", result)
+print("Max is: ", result)
+print("Min is: ", progMin(0,n,nums))
