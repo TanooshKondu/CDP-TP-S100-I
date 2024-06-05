c508e0ee9695e1c7dbd2c9582a80626bf323da21
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-06-05 11:29:44 +0530

    updated

--- Day-3/SudokuSolver.py
+++ Day-3/SudokuSolver.py
@@ -5,10 +5,10 @@ def printMatrix(x, y, board):
     if x == 9:
         print("Traversal completed")
         return 
- 
+
     print(board[x][y], end = " ")
     nextX, nextY = -1, -1 
- 
+
     if y == 8:
         nextX = x + 1 
         nextY = 0 
@@ -16,15 +16,15 @@ def printMatrix(x, y, board):
     else:
         nextX = x 
         nextY = y + 1
- 
+
     printMatrix(nextX, nextY, board)
- 
- 
+
+
 board = []
 for i in range(9):
     row = []
     for j in range(1, 10):
         row.append(j)
     board.append(row)
- 
+
 printMatrix(0, 0, board)
