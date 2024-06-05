f7d086a6dd8fa043d35170cb78c20bd68984393c
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-06-05 14:21:58 +0530

    updated

--- Day-3/N-Queens.py
+++ Day-3/N-Queens.py
@@ -1,7 +1,6 @@
 #https://pastebin.com/VjsB98M6
 
 class Solution(object):
-     
     def isPossible(self, row, col, board, n):
         # Check-1 --> same row, left cols
         prevCol = col - 1
@@ -9,7 +8,7 @@ class Solution(object):
             if board[row][prevCol] == 1:
                 return False 
             prevCol -= 1
- 
+
         # Check-2 --> left upper diagnol 
         prevRow, prevCol = row - 1, col - 1 
         while prevRow >= 0 and prevCol >= 0:
@@ -17,7 +16,7 @@ class Solution(object):
                 return False 
             prevRow -= 1 
             prevCol -= 1
- 
+
         # Check-3 --> left lower diagnol
         nextRow, prevCol = row + 1, col - 1 
         while nextRow < n and prevCol >= 0:
@@ -25,12 +24,11 @@ class Solution(object):
                 return False 
             nextRow += 1 
             prevCol -= 1
- 
+
         return True
- 
- 
- 
- 
+
+
+
     def solveThis(self, col, n, board, result):
         if col == n:
             # append to result
@@ -45,15 +43,15 @@ class Solution(object):
                 currResult.append(word)
             result.append(currResult)
             return
- 
+
         for row in range(n):
             if self.isPossible(row, col, board, n) == True:
                 board[row][col] = 1 
                 self.solveThis(col + 1, n, board, result)
                 board[row][col] = 0
- 
- 
- 
+
+
+
     def solveNQueens(self, n):
         result = []
         board = []
@@ -62,7 +60,6 @@ class Solution(object):
             for j in range(n):
                 row.append(0)
             board.append(row)
- 
+
         self.solveThis(0, n, board, result)
         return result
- 
