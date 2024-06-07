cedbab19eecb254886969d0fa2752cf20834a969
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-06-07 12:58:59 +0530

    updated

--- Day-5/PathStepCount.py
+++ Day-5/PathStepCount.py
@@ -1,7 +1,10 @@
 #LEETCODE 70
 def findPathCount(i, n):
-    if i == n:
+    if n == 1:
         return 1
-    if i >n:
-        return 0
+    if n == 2:
+        return 2
+    return findPathCount(n-1) + findPathCount(n-2)
     
+n = int(input())
+print(findPathCount(n))
