024c21746b9777f0291c689896c790dfef759ee8
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-06-07 15:05:46 +0530

    updated

--- Day-5/MinimumCost.py
+++ Day-5/MinimumCost.py
@@ -33,8 +33,11 @@ class Solution(object):
         # option2 = solveIt(1, n, cost)
         # return min(option1, option2)
         
-        
+#LEETCODE - 198        
 #using recursion find if str is palindrome or not
 #check whether 2 str's same or not
-#check whether one str is substr of other one or not
-#(n-1, n-1) to (0,0)
+#Check whether one string is a subsequence of the other or not using recursion.
+#Starting from (n-1, n-1), reach till (0, 0) in a n * n square matrix, and print all the elements using recursion (similar to sudoku solver)
+#Find all the digits sum within a given number using recursion.
+
+
