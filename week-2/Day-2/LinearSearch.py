1172a03727bdca29ca0d0d8cd597a3f4cfa3e818
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-06-11 14:06:20 +0530

    updated

--- week-2/Day-2/LinearSearch.py
+++ week-2/Day-2/LinearSearch.py
@@ -8,4 +8,9 @@ def LinearSearch(nums,index,target,n):
     
 nums = [12, 34, 21, 23, 44, 56];
 target = 23 
-print(LinearSearch(nums,0,target, len(nums)))
+index = LinearSearch(nums,0,target, len(nums))
+if index == -1:
+    print(target, "not found")
+else:
+    print(target, "found at index:", index)
+
