a2eb4b7f563189878a678981bd92e696cf5f589b
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-06-11 14:18:05 +0530

    updated

---
+++ week-2/Day-2/BinarySearch.py
@@ -0,0 +1,23 @@
+def BinarySearch(nums,target,n,found,left,right):
+
+    n = len(nums)
+    if left <= right:
+        mid = (left + right) // 2 
+        if nums[mid] == target:
+            return True 
+        elif nums[mid] < target:
+            return BinarySearch(nums,target,n,found,mid+1,right)
+        else:
+            return BinarySearch(nums, target, n, found,left,mid-1)
+    
+            
+nums = [12, 34, 21, 23, 44, 56];
+target = 23
+nums = sorted(nums)
+
+BinarySearch(nums,target,len(nums),False,0,len(nums)-1)
+found = False
+if  True:
+    print("Target found")
+else:
+    print("Target Not found")
