24afa831b5fdb1ca1d58379dec8e8851cd4c2f86
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-06-11 09:56:50 +0530

    updated

--- week-2/Day-2/BubbleSort.py
+++ week-2/Day-2/BubbleSort.py
@@ -9,5 +9,17 @@ def BubbleSort(nums):
         return nums
         pos -= 1
     
+#nums = [1, 4, 2, 16]
+#print(BubbleSort(nums))
+
+
+def BubbleSortRecc(nums,index1,index2):
+    n = len(nums)
+    if index1 == n or index2 == n:
+        return 
+    if nums[index1]> nums[index2]:
+        nums[index1], nums[index2] = nums[index2], nums[index1]
+    return BubbleSortRecc(nums, index1+1, index2+1)
 nums = [1, 4, 2, 16]
-print(BubbleSort(nums))
+BubbleSortRecc(nums,0,1)
+print(nums)
