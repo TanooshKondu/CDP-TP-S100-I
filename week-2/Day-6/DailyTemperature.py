1603e609e89594c17e2c2b3f6a48321bfbf0d222
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-06-15 11:06:15 +0530

    updated

---
+++ week-2/Day-6/DailyTemperature.py
@@ -0,0 +1,3 @@
+#leetcode-739
+
+#https://hastebin.com/share/neyenuvoxo.python
---
+++ week-2/Day-6/evaluate-reverse-polish-notation.py
@@ -0,0 +1,26 @@
+#https://hastebin.com/share/tapozojowa.python
+
+#leetcode-150
+class Solution(object):
+    def evalRPN(self, tokens):
+        stack = []  
+        operators = ['+', '-', '*', '/']
+        for ele in tokens:
+            if ele not in operators:
+                stack.append(int(ele))
+            else:
+                
+                num2 = stack.pop()
+                num1 = stack.pop()
+                if ele == '+':
+                    result = num1 + num2 
+                elif ele == '-':
+                    result = num1 - num2 
+                elif ele == '*':
+                    result = num1 * num2 
+                else:
+                    result = float(num1) / float(num2)
+                    result = int(result) 
+                stack.append(result)
+
+        return stack[0]
