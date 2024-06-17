79a0c396d847ba466f03240dadc756006d5df493
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-06-17 21:42:50 +0530

    updated

--- week-3/Day-1/LowestCommonAncestorOfaBST.py
+++ week-3/Day-1/LowestCommonAncestorOfaBST.py
@@ -1,2 +1,17 @@
 #LeEtcode-235
 #https://pastebin.com/1PyXQwGr
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        if not root:
+            return None 
+
+        currNode = root 
+        while currNode:
+            if currNode.val > p.val and currNode.val > q.val:
+                currNode = currNode.left 
+            elif currNode.val < p.val and currNode.val < q.val:
+                currNode = currNode.right 
+            else:
+                return currNode 
+        return None
