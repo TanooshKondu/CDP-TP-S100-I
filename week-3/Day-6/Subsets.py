c9cc1a1e672be06fca0c4b0281fe20f4709357d3
Tanoosh Kondu <135498186+TanooshKondu@users.noreply.github.com>
2024-06-22 14:26:13 +0530

    updated

---
+++ week-3/Day-6/Subsets.py
@@ -0,0 +1,16 @@
+#LEETcode-78
+
+#https://pastebin.com/fYbYLAWq
+class Solution(object):
+    def subsets(self, nums):
+        result = []
+        n = len(nums)
+        N = 1 << n
+        for i in range(N):
+            curr = []
+            # 101
+            for j in range(31):
+                if i & (1 << j):
+                    curr.append(nums[j])
+            result.append(curr)
+        return result
